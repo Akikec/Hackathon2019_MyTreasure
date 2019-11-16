c9efe7b1d8dff072fece49221c2bc3b8b8131d10
Akikec <bullyboy131@gmail.com>
2019-11-16 14:38:25 +0300

    planets

---
+++ Assets/Collectable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //var col = other.gameObject.GetComponent<Collectable>();
+        //var y = col.lenght * 5;
+        var y = 5;
+
+        var newVector = Vector3.zero;
+        newVector.y = y;
+        gameObject.transform.parent = other.gameObject.transform;
+        gameObject.transform.position = newVector;
+        this.enabled = false;
+    }
+}
