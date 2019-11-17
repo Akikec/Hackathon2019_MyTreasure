8f7d4d0834776a700e8c4fadc5aa0749b7acf36e
berrykot <berrykot@gmail.com>
2019-11-17 14:38:31 +0300

    Rotation

--- Assets/Scripts/WorldRotation.cs
+++ Assets/Scripts/WorldRotation.cs
@@ -2,33 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigitBodyGravitation : MonoBehaviour
+public class WorldRotation : MonoBehaviour
 {
 	[SerializeField]
 	protected Transform gravityTo;
-	[SerializeField]
-	protected float gravityScale;
-	[SerializeField]
-	protected float minGroundedDistance;
-	protected Rigidbody rb;
 	protected Vector3 DownDirection { get => gravityTo != null ? gravityTo.position - transform.position : transform.position; }
 	protected Vector3 UpDirection { get => DownDirection * -1; }
 	protected Vector3 GravitiDirection { get => gravityTo != null ? ClosestGravityPoint(transform.position) - transform.position : transform.position; }
-	public bool IsGrounded { get => GravitiDirection.sqrMagnitude <= minGroundedDistance * minGroundedDistance; }
 
-	void Start()
-	{
-		rb = GetComponent<Rigidbody>();
-	}
 	void FixedUpdate()
 	{
-		AddGravityForce();
-	}
-
-	protected void AddGravityForce()
-	{
-		if (!IsGrounded)
-			rb?.AddForce(GravitiDirection.normalized * gravityScale * rb.mass);
+		SetRotationUp();
 	}
 
 	protected Vector3 ClosestGravityPoint(Vector3 position)
@@ -42,5 +26,12 @@ public class RigitBodyGravitation : MonoBehaviour
 			}
 		}
 		return gravityTo.position;
+	}
+
+	protected void SetRotationUp()
+	{
+		var _direction = UpDirection.normalized;
+		var _lookRotation = Quaternion.LookRotation(_direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * 1);
 	}
 }
